9dde7c8f255bb60695c9bfb0ab3f35987e95fc9c
Syahruddin <syahruddinr@gmail.com>
2019-10-21 19:20:35 +0700

    entah sudah jadi atau belum

--- app.py
+++ app.py
@@ -9,13 +9,31 @@ def fungsikalender():
     elif request.method == 'POST':
         date = request.args.get('date', '')
         hari = request.args.get('hari', '')
-        isHaribesar = request.args.get('isHaribesar', '')
         haribesar = request.args.get('haribesar', '')
         
-        return newday(date,hari,isHaribesar,haribesar)
+        return newday(date,hari,haribesar)
 
 @app.route('/API-Kalender-Hari-Besar', method = ['GET'])
 
 def fungsihari(date):
     if request.method == 'GET':
-        return get_kalender(date)
+        return get_hari(date)
+        
+        
+def get_kalender():
+    kalender = session.query(Day).all()
+    return jsonify(kalender= [d.serialize for d in kalender])
+    
+def get_hari(date_dicari):
+    hari = session.query(Day).filter_by(date = date_dicari).one()
+    return jsonify(hari= hari.serialize)
+    
+def newday(date,hari,haribesar)
+    if haribesar == "":
+        isHaribesar = False
+    else
+        isHaribesar = True
+        
+    haribaru = Day(date=date, hari=hari, haribesar=haribesar, isHaribesar=isHaribesar)
+    session.add(haribaru)
+    return jsonify(hari=haribaru.serialize)
